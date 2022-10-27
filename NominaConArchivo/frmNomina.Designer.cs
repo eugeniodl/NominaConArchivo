7afe58a8ac5674878e3978f9c85839a860933e21
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2022-10-27 12:02:59 -0600

    Agregar archivos de proyecto.

---
+++ NominaConArchivo/frmNomina.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace NominaConArchivo
+{
+    partial class frmNomina
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblArchivo = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.ofdDialogo = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblArchivo
+            // 
+            this.lblArchivo.AutoSize = true;
+            this.lblArchivo.Location = new System.Drawing.Point(12, 20);
+            this.lblArchivo.Name = "lblArchivo";
+            this.lblArchivo.Size = new System.Drawing.Size(46, 13);
+            this.lblArchivo.TabIndex = 0;
+            this.lblArchivo.Text = "&Archivo:";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArchivo.Location = new System.Drawing.Point(64, 17);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(485, 20);
+            this.txtArchivo.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscar.Location = new System.Drawing.Point(555, 15);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcesar.Location = new System.Drawing.Point(636, 15);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 3;
+            this.btnProcesar.Text = "&Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.BtnProcesar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResultado.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 44);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResultado.Size = new System.Drawing.Size(699, 361);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // ofdDialogo
+            // 
+            this.ofdDialogo.Title = "Seleccione un archivo";
+            // 
+            // frmNomina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 417);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.lblArchivo);
+            this.Name = "frmNomina";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nómina";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblArchivo;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.OpenFileDialog ofdDialogo;
+    }
+}
+
---
+++ NominaConArchivo/frmNomina.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NominaConArchivo
+{
+    public partial class frmNomina : Form
+    {
+        public frmNomina()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProcesar_Click(object sender, EventArgs e)
+        {
+            if (txtArchivo.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un archivo", "Nómina",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtResultado.AppendText(string.Format("{0,-35} {1,12} {2,12} {3,12}" + Environment.NewLine, "NOMBRES Y APELLIDOS", "H.TRABAJADAS", "V.HORA", "V.BRUTO"));
+
+            StreamReader lector = new StreamReader(txtArchivo.Text);
+            
+            string registro = lector.ReadLine();
+            while(registro != null)
+            {
+                int pos = registro.IndexOf(',');
+                string nombre = registro.Substring(0, pos);
+                registro = registro.Substring(pos + 1);
+                pos = registro.IndexOf(',');
+                int horas = Convert.ToInt32(registro.Substring(0, pos));
+                registro = registro.Substring(pos + 1);
+                pos = registro.IndexOf(',');
+                decimal valor = Convert.ToDecimal(registro.Substring(0, pos));
+                decimal sbruto = horas * valor;
+
+                txtResultado.AppendText(string.Format("{0,-35} {1,10} {2,12:C} {3,12:C}" + Environment.NewLine, nombre, horas, valor, sbruto));
+                registro = lector.ReadLine();
+            }
+            lector.Close();
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            DialogResult rta = ofdDialogo.ShowDialog();
+            if (rta == DialogResult.Cancel) return;
+            txtArchivo.Text = ofdDialogo.FileName;
+        }
+    }
+}
